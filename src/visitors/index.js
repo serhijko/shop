2dbb77402dac175da566a16eebb38f9e02bee1e2
serhijko <serhijko>
2019-10-03 05:56:19 +0300

    Add VisitorCreate

--- src/visitors/index.js
+++ src/visitors/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
 import Icon from '@material-ui/icons/Person';
 
 import VisitorList from './VisitorList';
+import VisitorCreate from './VisitorCreate';
 import VisitorEdit from './VisitorEdit';
 
 export const VisitorIcon = Icon;
 
 export default {
     list: VisitorList,
+    create: VisitorCreate,
     edit: VisitorEdit,
     icon: VisitorIcon,
 }
