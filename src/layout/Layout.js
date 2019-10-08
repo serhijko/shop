dd44e47ab933fd406a41d2caadb0d1fb46e45159
serhijko <sp_by@tut.by>
2019-10-08 22:37:22 +0300

    Add custom AppBar with Logo

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { Layout } from 'react-admin';
-
+import AppBar from './AppBar';
 import { darkTheme } from './themes';
+
+const CustomLayout = props => <Layout {...props} appBar={AppBar} />;
 
 export default connect(state => ({
     theme: state.theme === 'dark' ? darkTheme : {},
-}))(Layout);
+}))(CustomLayout);
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,6 @@
+import AppBar from './AppBar';
 import Layout from './Layout';
 import Login from './Login';
 import Menu from './Menu';
 
-export { Layout, Login, Menu };
+export { AppBar, Layout, Login, Menu };
