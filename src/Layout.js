67fd2cbce4c10bdc0b5d9a687804e526345f3b6f
serhijko <sp_by@tut.by>
2019-10-08 19:15:55 +0300

    Add lightTheme

--- src/Layout.js
+++ src/Layout.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { Layout } from 'react-admin';
 
-import { darkTheme } from './theme';
+import { darkTheme } from './layout/themes';
 
 export default connect(state => ({
     theme: state.theme === 'dark' ? darkTheme : {},
--- src/theme.js
+++
@@ -1,5 +0,0 @@
-export const darkTheme = {
-    palette: {
-        type: 'dark', // Switching the dark mode on is a single property value change.
-    },
-};
